72cdcf281dd4d3ee6d10b28966b2d5a7e4b32a74
Gianfranco Raselli <66041020+GianfrancoRaselli@users.noreply.github.com>
2022-08-29 16:28:19 -0300

    createFund test

--- ethereum/test/tests.js
+++ ethereum/test/tests.js
@@ -1,18 +1,30 @@
 const { expect } = require('chai');
 const { ethers } = require('hardhat');
 
-describe('FundFactory', function () {
+describe('FundFactory', function() {
   let fundFactory;
-  let deploymentAccount;
 
-  beforeEach(async function () {
+  beforeEach(async function() {
+    const FundFactory = await ethers.getContractFactory('FundFactory');
+    fundFactory = await FundFactory.deploy();
+  });
+
+  it('Contract deployed', async function() {
+    expect(await fundFactory.address).to.not.equal(null || undefined);
+  });
+});
+
+describe('Fund', function() {
+  let fund;
+
+  it('Create fund', async function() {
     const FundFactory = await ethers.getContractFactory('FundFactory');
     fundFactory = await FundFactory.deploy();
 
-    deploymentAccount = (await ethers.getSigners())[0];
-  });
+    const createFundTx = await fundFactory.createFund('Name', 'Description', [(await ethers.getSigners())[0].address], true, true, false, false, 5, 50);
+    await createFundTx.wait();
 
-  it('Contract deployed', async function () {
     expect(await fundFactory.address).to.not.equal(null || undefined);
+    expect(await fundFactory.getDeployedFundsCount()).to.equal(1);
   });
 });
