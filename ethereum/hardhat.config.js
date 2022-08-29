e2e1dda5d1172762513f69c9ea3f7f3f1d8cd91f
Gianfranco Raselli <66041020+GianfrancoRaselli@users.noreply.github.com>
2022-08-28 21:58:27 -0300

    new gas reporter rule

--- ethereum/hardhat.config.js
+++ ethereum/hardhat.config.js
@@ -1,49 +1,50 @@
-require("@nomiclabs/hardhat-waffle");
-require("@nomiclabs/hardhat-etherscan");
-require("@nomiclabs/hardhat-solhint");
-require("solidity-coverage");
-require("hardhat-tracer");
-require("hardhat-gas-reporter");
-require("hardhat-watcher");
-require("hardhat-spdx-license-identifier");
+require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-etherscan');
+require('@nomiclabs/hardhat-solhint');
+require('solidity-coverage');
+require('hardhat-tracer');
+require('hardhat-gas-reporter');
+require('hardhat-watcher');
+require('hardhat-spdx-license-identifier');
 
-require("dotenv").config();
+require('dotenv').config();
 
 module.exports = {
-  solidity: "0.8.16",
+  solidity: '0.8.16',
   paths: {
-    artifacts: "./artifacts",
-    sources: "./contracts",
-    cache: "./cache",
-    tests: "./test"
+    artifacts: './artifacts',
+    sources: './contracts',
+    cache: './cache',
+    tests: './test',
   },
   //defaultNetwork: "ganache",
   networks: {
     ganache: {
-      url: "http://127.0.0.1:7545"
+      url: 'http://127.0.0.1:7545',
     },
     goerli: {
       url: `https://goerli.infura.io/v3/${process.env.INFURA_KEY}`,
-      accounts: [process.env.PRIVATE_KEY]
-    }
+      accounts: [process.env.PRIVATE_KEY],
+    },
   },
   etherscan: {
     apiKey: {
-      goerli: process.env.ETHERSCAN_KEY
+      goerli: process.env.ETHERSCAN_KEY,
     },
     customChains: [
       {
-        network: "goerli",
+        network: 'goerli',
         chainId: 5,
         urls: {
-          apiURL: "https://api-goerli.etherscan.io/api",
-          browserURL: "https://goerli.etherscan.io"
-        }
-      }
-    ]
+          apiURL: 'https://api-goerli.etherscan.io/api',
+          browserURL: 'https://goerli.etherscan.io',
+        },
+      },
+    ],
   },
   gasReporter: {
-    enabled: (process.env.REPORT_GAS === "true") ? true : false
+    enabled: process.env.REPORT_GAS === 'true' ? true : false,
+    currency: 'USD',
   },
   watcher: {
     compilation: {
@@ -55,11 +56,7 @@ module.exports = {
       start: 'echo Running my compilation task now...',
     },
     ci: {
-      tasks: [
-        'clean',
-        { command: 'compile', params: { quiet: true } },
-        { command: 'test', params: { noCompile: true } },
-      ],
+      tasks: ['clean', { command: 'compile', params: { quiet: true } }, { command: 'test', params: { noCompile: true } }],
     },
   },
   /*spdxLicenseIdentifier: {
