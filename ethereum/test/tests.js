a91e3b93cab718ae54da823dd751193c503901e4
Gianfranco Raselli <66041020+GianfrancoRaselli@users.noreply.github.com>
2022-09-02 14:36:23 -0300

    refactor tests

--- ethereum/test/tests.js
+++ ethereum/test/tests.js
@@ -1,10 +1,18 @@
 const { expect } = require('chai');
 const { ethers } = require('hardhat');
 
+let FundFactory;
+let Fund;
 let fundFactory;
+let defaultCreateFundArgs;
+
+before(async function() {
+  FundFactory = await ethers.getContractFactory('FundFactory');
+  Fund = await ethers.getContractFactory('Fund');
+  defaultCreateFundArgs = ['Name', 'Description', [(await ethers.getSigners())[0].address]];
+});
 
 beforeEach(async function() {
-  const FundFactory = await ethers.getContractFactory('FundFactory');
   fundFactory = await FundFactory.deploy();
 });
 
@@ -16,37 +24,42 @@ describe('FundFactory contract', function() {
 
   describe('Create fund', function() {
     it('Incorrect contribution percentage', async function() {
-      const args = ['Name', 'Description', [(await ethers.getSigners())[0].address], true, true, true, false, false, 105, 50];
+      const createFundArgs = [...defaultCreateFundArgs, true, true, true, false, false, 105, 50];
 
-      await expect(fundFactory.createFund(...args)).to.be.revertedWith('Incorrect contribution percentage');
+      await expect(fundFactory.createFund(...createFundArgs)).to.be.revertedWith('Incorrect contribution percentage');
       expect(await fundFactory.getDeployedFundsCount()).to.equal(0);
     });
 
     it('Incorrect approvals percentage', async function() {
-      const args = ['Name', 'Description', [(await ethers.getSigners())[0].address], true, true, true, false, false, 5, 105];
+      const createFundArgs = [...defaultCreateFundArgs, true, true, true, false, false, 5, 105];
 
-      await expect(fundFactory.createFund(...args)).to.be.revertedWith('Incorrect approvals percentage');
+      await expect(fundFactory.createFund(...createFundArgs)).to.be.revertedWith('Incorrect approvals percentage');
       expect(await fundFactory.getDeployedFundsCount()).to.equal(0);
     });
 
     it('Create new fund', async function() {
-      const args = ['Name', 'Description', [(await ethers.getSigners())[0].address], true, true, true, false, false, 5, 50];
-      const createFundTx = await fundFactory.createFund(...args);
-      
+      const createFundArgs = [...defaultCreateFundArgs, true, true, true, false, false, 5, 50];
+      const createFundTx = await fundFactory.createFund(...createFundArgs);
+
       await expect(createFundTx)
         .to.emit(fundFactory, 'NewFund')
-        .withArgs(args[0], args[1], (await ethers.getSigners())[0].address, (await ethers.provider.getBlock(createFundTx.blockNumber)).timestamp);
+        .withArgs(
+          createFundArgs[0],
+          createFundArgs[1],
+          (await ethers.getSigners())[0].address,
+          (await ethers.provider.getBlock(createFundTx.blockNumber)).timestamp,
+        );
       // Not supported yet
       /*
       .withNamedArgs({
-        name: args[0],
-        description: args[1],
+        name: createFundArgs[0],
+        description: createFundArgs[1],
         creator: (await ethers.getSigners())[0].address,
         createdAt: createFundTx.timestamp,
       })
     */
       // Not supported yet
-      //expect('createFund').to.be.calledOnContractWith(fundFactory, [...args]);
+      //expect('createFund').to.be.calledOnContractWith(fundFactory, [...createFundArgs]);
       expect(await fundFactory.getDeployedFundsCount()).to.equal(1);
       expect(await fundFactory.deployedFunds(0)).to.equal((await fundFactory.getDeployedFunds())[0]);
     });
@@ -54,14 +67,13 @@ describe('FundFactory contract', function() {
 });
 
 describe('Fund contract', function() {
+  let fund;
+
   describe('Friends fund', function() {
-    let fund;
-
     beforeEach(async function() {
-      const args = ['Name', 'Description', [(await ethers.getSigners())[0].address], true, true, true, false, false, 0, 50];
-      await fundFactory.createFund(...args);
+      const createFundArgs = [...defaultCreateFundArgs, true, true, true, false, false, 0, 50];
+      await fundFactory.createFund(...createFundArgs);
 
-      const Fund = await ethers.getContractFactory('Fund');
       fund = Fund.attach(await fundFactory.deployedFunds(0));
     });
 
@@ -73,13 +85,10 @@ describe('Fund contract', function() {
   });
 
   describe('Campaign fund', function() {
-    let fund;
-
     beforeEach(async function() {
-      const args = ['Name', 'Description', [(await ethers.getSigners())[0].address], false, false, true, true, true, 5, 50];
-      await fundFactory.createFund(...args);
+      const createFundArgs = [...defaultCreateFundArgs, false, false, true, true, true, 5, 50];
+      await fundFactory.createFund(...createFundArgs);
 
-      const Fund = await ethers.getContractFactory('Fund');
       fund = Fund.attach(await fundFactory.deployedFunds(0));
     });
 
@@ -91,13 +100,10 @@ describe('Fund contract', function() {
   });
 
   describe('Donation fund', function() {
-    let fund;
-
     beforeEach(async function() {
-      const args = ['Name', 'Description', [(await ethers.getSigners())[0].address], true, true, true, true, true, 5, 50];
-      await fundFactory.createFund(...args);
+      const createFundArgs = [...defaultCreateFundArgs, true, true, true, true, true, 5, 50];
+      await fundFactory.createFund(...createFundArgs);
 
-      const Fund = await ethers.getContractFactory('Fund');
       fund = Fund.attach(await fundFactory.deployedFunds(0));
     });
 
