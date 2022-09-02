682ee616c28d7c7e2ffd1c06db926956cbfceddd
Gianfranco Raselli <66041020+GianfrancoRaselli@users.noreply.github.com>
2022-09-02 16:54:53 -0300

    createFund script

---
+++ ethereum/scripts/transactions/createFund.js
@@ -0,0 +1,32 @@
+const hre = require('hardhat');
+const fs = require('fs');
+
+async function main() {
+  const FundFactory = await hre.ethers.getContractFactory('FundFactory');
+  const fundFactory = FundFactory.attach(
+    fs
+      .readFileSync('.lastAddressDeployed')
+      .toString()
+      .trim(),
+  );
+
+  await fundFactory.createFund(
+    'Name',
+    'Description',
+    [(await ethers.getSigners())[0].address],
+    true,
+    true,
+    true,
+    false,
+    false,
+    5,
+    50,
+  );
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
