89693da13b27200d07fd04da95a36a6041d33c4a
Gianfranco Raselli <66041020+GianfrancoRaselli@users.noreply.github.com>
2022-09-04 19:09:48 -0300

    fund token added

---
+++ ethereum/contracts/FundToken.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: ISC
+
+// Solidity compiler version
+pragma solidity 0.8.16;
+
+// Imports
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+// Token contract
+contract FundToken is Ownable, ERC20 {
+  constructor() ERC20("Fund Token", "FT") {}
+
+  function decimals() public pure override returns (uint8) {
+    return 18;
+  }
+
+  function mint(address _account, uint256 _amount) public onlyOwner {
+    _mint(_account, _amount);
+  }
+
+  function burn(address _account, uint256 _amount) public onlyOwner {
+    _burn(_account, _amount);
+  }
+}
