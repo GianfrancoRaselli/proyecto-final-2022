7391879ac64b65542dfdaf3f1c6803c50e19acf5
Gianfranco Raselli <66041020+GianfrancoRaselli@users.noreply.github.com>
2022-09-09 01:43:18 -0300

    setTimeout in notifications

--- UIWeb/src/composables/useNotifications.js
+++ UIWeb/src/composables/useNotifications.js
@@ -1,24 +1,32 @@
 import { reactive } from 'vue';
 
 const notifications = reactive([]);
+let id = 0;
 
 const addNotification = ({ message, timeout = 15000, type = 'info' }) => {
-  const id = Math.random() + Date.now();
-  notifications.push({ id, message, type });
-  if (timeout) {
-    const removeNotificationSubscription = setTimeout(() => {
-      removeNotification(id);
-      clearTimeout(removeNotificationSubscription);
-    }, timeout);
-  }
+  notifications.push({ id, message, type, subscription: addTimeout(id, timeout) });
+  id++;
 };
+
+function addTimeout(id, timeout) {
+  if (timeout)
+    return setTimeout(() => {
+      removeNotification(id);
+    }, timeout);
+}
 
 const removeNotification = (id) => {
   const index = notifications.findIndex((item) => item.id === id);
-  notifications.splice(index, 1);
+  if (index >= 0) {
+    if (notifications[index].subscription) clearTimeout(notifications[index].subscription);
+    notifications.splice(index, 1);
+  }
 };
 
 const removeNotifications = () => {
+  notifications.forEach((notification) => {
+    if (notification.subscription) clearTimeout(notification.subscription);
+  });
   notifications.splice(0);
 };
 
