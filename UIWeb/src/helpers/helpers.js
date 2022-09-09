dc926e71f8c098227f48e6aad2ad67d24f4c5fbb
Gianfranco Raselli <66041020+GianfrancoRaselli@users.noreply.github.com>
2022-09-09 13:22:35 -0300

    check if exists the contractInstance

--- UIWeb/src/helpers/helpers.js
+++ UIWeb/src/helpers/helpers.js
@@ -7,12 +7,14 @@ import fundTokenABI from '../assets/abis/FundToken';
 import { fundTokenAddress } from '../assets/lastAddresses';
 
 const call = async (contract, method, params = [], options) => {
-  const contractInstance = await getContractInstance(contract, 'call');
-  try {
-    return contractInstance.methods[method](...params).call({ from: store.state.connection.address, ...options });
-  } catch (err) {
-    showErrorNotification(err);
-    throw err;
+  const contractInstance = await getContractInstance(contract, 'infura');
+  if (contractInstance) {
+    try {
+      return contractInstance.methods[method](...params).call({ from: store.state.connection.address, ...options });
+    } catch (err) {
+      showErrorNotification(err);
+      throw err;
+    }
   }
 };
 
@@ -20,14 +22,16 @@ const transaction = async (contract, method, params = [], options) => {
   if (hasMetamask()) {
     if (store.getters.isConnected) {
       if (store.getters.isConnectedToTheValidChain) {
-        const contractInstance = await getContractInstance(contract, 'transaction');
-        try {
-          await contractInstance.methods[method](...params).call({ from: store.state.connection.address, ...options });
-        } catch (err) {
-          showErrorNotification(err);
-          throw err;
+        const contractInstance = await getContractInstance(contract);
+        if (contractInstance) {
+          try {
+            await contractInstance.methods[method](...params).call({ from: store.state.connection.address, ...options });
+          } catch (err) {
+            showErrorNotification(err);
+            throw err;
+          }
+          return contractInstance.methods[method](...params).send({ from: store.state.connection.address, ...options });
         }
-        return contractInstance.methods[method](...params).send({ from: store.state.connection.address, ...options });
       } else {
         addNotification({
           message: 'You have to be connected to ' + store.getters.validChainName + ' to send a transaction',
@@ -50,21 +54,23 @@ const transaction = async (contract, method, params = [], options) => {
 };
 
 const event = async (contract, event, options, func) => {
-  const contractInstance = await getContractInstance(contract, 'event');
+  const contractInstance = await getContractInstance(contract);
   if (contractInstance) return contractInstance.events[event]({ fromBlock: 'latest', ...options }, func);
 };
 
-async function getContractInstance(contract, operation) {
-  if (operation === 'call') {
+async function getContractInstance(contract, provider = 'metamask') {
+  if (provider === 'infura') {
     if (contract === 'FundFactory') return store.state.connection.infuraFundFactory;
     if (contract === 'FundToken') return new store.state.connection.infuraWeb3.eth.Contract(fundTokenABI, fundTokenAddress);
     const { default: contractABI } = await import('../assets/abis/' + contract.name);
     return new store.state.connection.infuraWeb3.eth.Contract(contractABI, contract.address);
-  } else {
-    if (contract === 'FundFactory') return store.state.connection.fundFactory;
-    if (contract === 'FundToken') return new store.state.connection.web3.eth.Contract(fundTokenABI, fundTokenAddress);
-    const { default: contractABI } = await import('../assets/abis/' + contract.name);
-    return new store.state.connection.web3.eth.Contract(contractABI, contract.address);
+  } else if (provider === 'metamask') {
+    if (store.state.connection.provider) {
+      if (contract === 'FundFactory') return store.state.connection.fundFactory;
+      if (contract === 'FundToken') return new store.state.connection.web3.eth.Contract(fundTokenABI, fundTokenAddress);
+      const { default: contractABI } = await import('../assets/abis/' + contract.name);
+      return new store.state.connection.web3.eth.Contract(contractABI, contract.address);
+    }
   }
 }
 
