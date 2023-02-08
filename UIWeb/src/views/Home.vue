468d7ce0c3d2667deaa5bca40243c4c3c22412c7
Gianfranco Raselli <66041020+GianfrancoRaselli@users.noreply.github.com>
2023-02-08 18:24:00 -0300

    about us

--- UIWeb/src/views/Home.vue
+++ UIWeb/src/views/Home.vue
@@ -1,25 +1,120 @@
 <template>
   <div class="content">
     <div class="main-content">
-      <img class="fund-img" src="@/assets/imgs/main-transparent.png" />
-      <div class="info">
-        <div class="header">
-          <p class="title">Transformando la forma de financiación</p>
-          <p class="description">
-            Brindamos seguridad y autocontrol a todos los que buscan una forma totalmente descentralizada de administrar fondos de
-            dinero compartidos utilizando los contratos inteligentes de la cadena de bloques de Ethereum.
-          </p>
+      <p class="title">Sistema de administración de fondos comunes</p>
+      <div class="sub-content">
+        <img class="fund-img" src="@/assets/imgs/main-transparent.png" />
+        <div class="info">
+          <div class="header">
+            <p class="subtitle">Transformando la forma de financiación</p>
+            <p class="description">
+              Brindamos seguridad y autocontrol a todos los que buscan una forma totalmente descentralizada de administrar fondos
+              de dinero compartidos utilizando los contratos inteligentes de la cadena de bloques de Ethereum.
+            </p>
+          </div>
+          <router-link class="button" :to="{ name: 'Funds', params: { animation: 'slide' } }">
+            <button class="btn btn-primary btn-discover">
+              <span>Descubrir fondos</span><fa-icon icon="arrow-right" class="img-icon ml-3"></fa-icon>
+            </button>
+          </router-link>
         </div>
-        <router-link class="button" :to="{ name: 'Funds', params: { animation: 'slide' } }">
-          <button class="btn btn-primary btn-discover">
-            <span>Descubrir fondos</span><fa-icon icon="arrow-right" class="img-icon ml-3"></fa-icon>
-          </button>
-        </router-link>
       </div>
     </div>
 
     <div class="we-content">
       <img src="@/assets/imgs/logos/1/Fund-logos_transparent.png" />
+      <p class="title">Acerca de nosotros</p>
+      <div class="questions">
+        <div class="question">
+          <p class="ask text-primary">¿Quiénes somos?</p>
+          <p class="answer">
+            La aplicación comenzó como proyecto llevado a cabo para la materia Proyecto Final de la carrera Ingeniería en Sistemas
+            de Información de la Universidad Tecnológica Nacional (UTN) ubicada en Rosario, Argentina.
+          </p>
+        </div>
+
+        <div class="question">
+          <p class="ask text-dark">¿Qúe tipo de aplicación es?</p>
+          <p class="answer">
+            El sistema de administración de fondos comunes es una aplicación descentralizada que funciona sobre la cadena de
+            bloques (Blockchain) de Ethereum.
+          </p>
+        </div>
+
+        <div class="question">
+          <p class="ask text-success">¿Cuál es la función de la aplicación?</p>
+          <p class="answer">
+            La función principal de la aplicación es llevar un registro de los activos (Ether) que las diferentes cuentas de
+            Ethereum (entidades) pongan a disposición para una causa común. Los mismos serán gestionados mediante contratos
+            inteligentes (Smart Contracts), que son porciones de código que residen en la cadena de bloques mediante los cuales se
+            implementó la lógica de negocio.
+          </p>
+        </div>
+
+        <div class="question">
+          <p class="ask text-danger">¿Qué usos se le puede dar a la aplicación?</p>
+          <p class="answer">
+            La aplicación cuenta con distintos usos prácticos, la misma permite gestionar los fondos de donaciones, pensiones,
+            seguros, campañas de inversiones, entre varios usos más que cada entidad le puede asignar personalizando diferentes
+            parámetros a su medidad durante la creación de los mismos.
+          </p>
+        </div>
+
+        <div class="question">
+          <p class="ask text-warning">¿Qué ventajas nos proporciona la cadena de bloques?</p>
+          <ul class="answer">
+            <li>
+              <span class="item">Descentralización</span>: una cadena de bloques descentralizada añade una red de igual a igual a
+              las características de seguridad existentes típicas de las bases de datos de una blockchain. Los miembros de esta
+              red no tienen que confiar ni conocerse entre sí, sino que cada integrante obtiene una copia del mismo registro de
+              contabilidad de la misma.
+            </li>
+            <li>
+              <span class="item">Transparencia</span>: la transparencia se consigue publicando las reglas con las que se define el
+              funcionamiento de la cadena de bloques. Esto se logra haciendo público el código del software necesario para
+              ejecutar la red y generando una comunidad de nodos y desarrolladores que siguen este principio de transparencia.
+            </li>
+            <li>
+              <span class="item">Inmutabilidad</span>: ningún participante puede cambiar o falsificar una transacción una vez
+              grabada en el libro mayor compartido. Si el registro de una transacción incluye un dato no deseado, se debe añadir
+              una nueva transacción para revertir el error, pero ambas transacciones serán visibles.
+            </li>
+            <li>
+              <span class="item">Privacidad</span>: las direcciones de las cuentas no están ligadas a las identidades de las
+              personas. Para poder operar en la cadenad e bloques es necesario disponer del par de claves pública y privada.
+            </li>
+            <li>
+              <span class="item">Trazabilidad</span>: la cadena de bloques permite realizar un recorrido de todas las operaciones
+              que se han realizado sobre una determinada dirección, o retroceder en el tiempo y revisar las transacciones que se
+              hicieron en una fecha determinada explorando todos los bloques generados en la fecha indicada.
+            </li>
+          </ul>
+        </div>
+
+        <div class="question">
+          <p class="ask text-info">¿Qué se necesita para comenzar a utilizar la aplicación?</p>
+          <p class="answer">
+            Todo lo que se necesita para comenzar a iteractuar con la aplicación es contar con la billetera MetaMask instalada en
+            el navegador.
+          </p>
+        </div>
+
+        <div class="question">
+          <p class="ask text-success">¿Para qué sirve un FundToken?</p>
+          <p class="answer">
+            El FundToken es la moneda interna que se puede comprar en la aplicación. La misma funciona como un ticket para poder
+            crear un fondo.
+          </p>
+        </div>
+
+        <div class="question">
+          <p class="ask text-danger">¿Quiénes pueden crear un fondo?</p>
+          <p class="answer">
+            Cualquier persona, entidad o empresa que disponga de una cuenta en la billetera de MetaMask puede crear e interactuar
+            con cualquier fondo.
+          </p>
+        </div>
+      </div>
     </div>
 
     <div class="community-content">
@@ -54,8 +149,8 @@
       <div class="text">
         <p class="title">¡Instala MetaMask!</p>
         <p class="description">
-          Por el momento, para poder comenzar a utilizar la aplicación debes tener instalada la billetera de MetaMask en el
-          navegador.
+          Por el momento, para poder comenzar a interactuar con la aplicación debes contar con la billetera de MetaMask instalada
+          en el navegador.
         </p>
         <p class="secondary-description">¡Estamos trabajando para incorporar nuevas formas de utilizar la aplicación!</p>
         <a href="https://metamask.io/download/" target="_blank"><button class="btn btn-primary">Instalar MetaMask</button></a>
@@ -83,52 +178,69 @@ export default {
 .main-content {
   min-height: calc(100vh - var(--navbar-height));
   padding: 1.5rem;
-  background: #4ac29a50; /* fallback for old browsers */
-  background: -webkit-linear-gradient(to bottom left, #ffffff, #4ac29a50); /* Chrome 10-25, Safari 5.1-6 */
+  background: #38849550; /* fallback for old browsers */
+  background: -webkit-linear-gradient(to bottom left, #38849520, #38849550); /* Chrome 10-25, Safari 5.1-6 */
   background: linear-gradient(
     to bottom left,
-    #ffffff,
-    #4aacc250
+    #38849520,
+    #38849550
   ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 
   display: flex;
-  flex-direction: row-reverse;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  flex-wrap: wrap;
-  gap: 35px;
+  gap: 2rem;
 
-  .fund-img {
-    height: 350px;
-    width: auto;
-    max-width: 100%;
+  .title {
+    font-size: 2.6rem;
+    font-weight: bold;
+    text-decoration: underline;
   }
 
-  .info {
-    font-size: 1.15rem;
-    text-align: center;
-    max-width: 667px;
-    animation: fadeInUpBig 1s;
+  .sub-content {
     display: flex;
-    flex-direction: column;
+    flex-direction: row-reverse;
     justify-content: center;
-    gap: 10px;
+    align-items: center;
+    flex-wrap: wrap;
+    gap: 2rem;
 
-    .header .title {
-      font-size: 1.9rem;
-      font-weight: bold;
-      text-decoration: underline;
+    .fund-img {
+      height: 350px;
+      width: auto;
+      max-width: 100%;
+    }
+
+    .info {
+      font-size: 1.15rem;
+      text-align: center;
+      max-width: 667px;
+      animation: fadeInUpBig 1s;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      gap: 10px;
+
+      .header {
+        .subtitle {
+          font-size: 1.8rem;
+          font-weight: bold;
+        }
+      }
     }
   }
 }
 
 .we-content {
+  padding: 3rem;
   background: #eae7e781; /* fallback for old browsers */
-  background: -webkit-linear-gradient(to top left, #2c3e5045, #eae7e781); /* Chrome 10-25, Safari 5.1-6 */
+  background: -webkit-linear-gradient(to left, #5c886241, #4d7a5558, #5c886241); /* Chrome 10-25, Safari 5.1-6 */
   background: linear-gradient(
-    to top left,
-    #2c3e5045,
-    #eae7e781
+    to left,
+    #5c886241,
+    #4d7a5558,
+    #5c886241
   ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 
   display: flex;
@@ -140,7 +252,48 @@ export default {
   img {
     height: 200px;
     max-width: 100%;
-    margin-bottom: 2rem;
+    margin-bottom: 1.5rem;
+  }
+
+  .title {
+    font-size: 2.4rem;
+    font-weight: bold;
+    text-decoration: underline;
+  }
+
+  .questions {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    gap: 1.5rem;
+
+    .question {
+      font-size: 1.1rem;
+      text-align: center;
+      max-width: 45rem;
+      padding: 1.4rem;
+      border-radius: 1rem;
+      background-color: rgba(255, 255, 255, 0.507);
+      box-shadow: rgba(0, 0, 0, 0.371) 4px 4px 4px 2px;
+
+      .ask {
+        font-size: 1.5em;
+        font-weight: bold;
+      }
+
+      .answer {
+        font-size: 1em;
+
+        li {
+          padding: 0.5rem;
+
+          .item {
+            font-weight: bold;
+          }
+        }
+      }
+    }
   }
 }
 
@@ -167,12 +320,16 @@ export default {
   }
 
   .text {
-    max-width: 600px;
-    margin-bottom: 2rem;
+    max-width: 680px;
+    margin-bottom: 1.5rem;
 
     .title {
-      font-size: 1.6rem;
+      font-size: 1.8rem;
       font-weight: bold;
+    }
+
+    .description {
+      font-size: 1.1rem;
     }
   }
 
